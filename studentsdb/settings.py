58af95b7b2152094393c3b46284887d02db06061
”Yevhenii” <andrieiev.ievgenii@gmail.com>
2017-12-23 18:51:56 +0200

    some changes + adding context_processors file

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -9,9 +9,15 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.conf import global_settings
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+    'django.core.context_processors.request',
+    'studentsdb.context_processors.students_proc',
+)
+PORTAL_URL = "http://localhost:8000"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
