899e92c1fed2d0eea7495dc6b7ef5adfb81269fb
”Yevhenii” <andrieiev.ievgenii@gmail.com>
2018-01-04 12:54:02 +0200

    new ammendments

---
+++ students/views/groups.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def groups_list(request):
+    groups = (
+        {'id': 1,
+         'name': u'G-1',
+         'leader': {'id': 1, 'name': u'Yevhenii Andrieiev'}},
+        {'id': 2,
+         'name': u'G-2',
+         'leader': {'id': 2, 'name': u'Yevhenii Andrieiev'}},
+        {'id': 3,
+         'name': u'G-3',
+         'leader': {'id': 3, 'name': u'Yevhenii Andrieiev'}},
+    )
+    return render(request, 'students/groups_list.html', {'groups': groups})
+
+def groups_add(request):
+    return HttpResponse('<h1>Group Add Form</h1>')
+
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
--- students/views/students.py
+++ students/views/students.py
@@ -3,8 +3,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Views for Students
-
 
 def students_list(request):
     students = (
@@ -24,34 +22,14 @@ def students_list(request):
          'ticket': 5332,
          'image': 'img/img3.jpg'},
     )
-    return render(request, "students/students_list.html", {'students': students})
-
+    return render(request, 'students/students_list.html',
+        {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
 
-
 def students_edit(request, sid):
     return HttpResponse('<h1>Edit Student %s</h1>' % sid)
 
-
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete Student %s</h1>' % sid)
-
-# Views for Groups
-
-
-def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
-
-
-def groups_add(request):
-    return HttpResponse('<h1>Group Add Form</h1>')
-
-
-def groups_edit(request, gid):
-    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
-
-
-def groups_delete(request, gid):
-    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
